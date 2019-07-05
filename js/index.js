cd05c1594458e4cdd3f33c51ca6f96ba1cb53088
Cathy Zhang <50335806+cathyzhang123@users.noreply.github.com>
2019-07-05 12:47:03 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,68 @@
+// DATA RESOURCES
+let dataResource = [
+    {
+        heading: "Solar Energy",
+        bodyText: "Solar energy is radiant light and heat from the Sun that is harnessed using a range of ever-evolving technologies such as solar heating, photovoltaics, solar thermal energy, solar architecture, molten salt power plants and artificial photosynthesis.  It is an important source of renewable energy and its technologies are broadly characterized as either passive solar or active solar depending on how they capture and distribute solar energy or convert it into solar power. Active solar techniques include the use of photovoltaic systems, concentrated solar power and solar water heating to harness the energy. Passive solar techniques include orienting a building to the Sun, selecting materials with favorable thermal mass or light-dispersing properties, and designing spaces that naturally circulate air. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum.",
+        imgUrl: "img/solarEnergy.jpg"
+    },
+    {
+        heading: "Water Energy",
+        bodyText: "Water energy power is power derived from the energy of falling water or fast running water, which may be harnessed for useful purposes. Since ancient times, hydropower from many kinds of watermills has been used as a renewable energy source for irrigation and the operation of various mechanical devices, such as gristmills, sawmills, textile mills, trip hammers, dock cranes, domestic lifts, and ore mills. A trompe, which produces compressed air from falling water, is sometimes used to power other machinery at a distance. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum.",
+        imgUrl: "img/waterEnergy.png"
+    },
+    {
+        heading: "Wind Energy",
+        bodyText: "Wind energy is the use of air flow through wind turbines to provide the mechanical power to turn electric generators and traditionally to do other work, like milling or pumping. Wind power is a sustainable and renewable alternative to burning fossil fuels, and has much less effect on the environment.  Wind farms consist of many individual wind turbines, which are connected to the electric power transmission network. Onshore wind is an inexpensive source of electric power, competitive with or in many places cheaper than coal or gas plants.  Onshore wind farms also have an impact on the landscape, as typically they need to be spread over more land than other power stations and need to be built in wild and rural areas, which can lead to industrialization of the countryside and habitat loss.  Offshore wind is steadier and stronger than on land and offshore farms have less visual impact, but construction and maintenance costs are considerably higher. Small onshore wind farms can feed some energy into the grid or provide electric power to isolated off-grid locations. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl  ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis, nunc ut tincidunt pulvinar, tortor mauris tempor lectus, sed ornare nisl arcu faucibus enim. Aenean vel porta libero, facilisis ullamcorper tellus. Nam fringilla lorem a feugiat convallis. Vestibulum auctor mauris nibh, quis rutrum mi aliquet non. Donec egestas enim purus, nec placerat sapien volutpat a. Mauris euismod orci tempus vehicula rhoncus. Vivamus condimentum libero nisi, non lacinia nisl consequat in. Nam aliquam tempor ligula, sit amet venenatis odio euismod ut. Cras vel nibh ex. Nam tincidunt, leo a laoreet condimentum, nisl ipsum consequat turpis, vitae gravida ex ligula quis leo. Etiam ut quam condimentum, fermentum erat et, accumsan quam. Aliquam ultrices sem quis dolor porttitor, at dictum risus elementum.",
+        imgUrl: "img/windEnergy.jpg"
+    }
+];
+
+let $imgLinks = document.querySelectorAll('button');
+let $caption = document.getElementById("caption");
+let $text = document.getElementById("text");
+let $img = document.getElementById("img");
+
+// EVENT-OBJECT IS ACCESSED 
+// AS THE FIRST PARAMETER OF THE EVENT-HANDLER 
+function eventHandler(ev){    
+    // fetch the clicked button
+    let clickedBtn = ev.target;    
+    // read the text inside the button
+    let btnText = clickedBtn.innerText;    
+      
+    if (btnText === "Solar Energy") {
+    	$caption.innerHTML = `<h3>${dataResource[0].heading}</h3>`;
+    	$text.innerHTML = `<p>${dataResource[0].bodyText}</p>`;        
+        $img.src=`${dataResource[0].imgUrl}`;
+        $img.alt=`${dataResource[0].heading}`;                              
+    } else if (btnText === "Water Energy") {
+        $caption.innerHTML = `<h3>${dataResource[1].heading}</h3>`;
+    	$text.innerHTML = `<p>${dataResource[1].bodyText}</p>`;
+    	$img.src=`${dataResource[1].imgUrl}`;
+        $img.alt=`${dataResource[1].heading}`;     
+    } else {
+        $caption.innerHTML = `<h3>${dataResource[2].heading}</h3>`;
+    	$text.innerHTML = `<p>${dataResource[2].bodyText}</p>`;
+    	$img.src=`${dataResource[2].imgUrl}`;
+        $img.alt=`${dataResource[2].heading}`;
+    }    
+}
+
+for(let i=0; i<$imgLinks.length; i++){
+    $imgLinks[i].addEventListener("click", eventHandler);
+}
+
+const links = document.querySelectorAll(".main-content button");
+function handleEvent(ev) {
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].hasAttribute("id")) {
+            links[i].removeAttribute("id");
+        }
+    }    
+    let currentItem = ev.target;
+    currentItem.setAttribute("id", "active-button");
+}
+
+for (let link of links) {
+    link.addEventListener("click", handleEvent);
+}
